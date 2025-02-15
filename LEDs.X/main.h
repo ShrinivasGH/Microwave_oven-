2ec2235cfa11c8249e18a3f76148f62688e84155
Shrinivas Habbu <116146676+ShrinivasGH@users.noreply.github.com>
2025-02-15 10:20:54 +0530

    Add files via upload

---
+++ LEDs.X/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define	MAIN_H
+
+#define LED_ARRAY PORTD
+#define LED_ARRAY_DDR TRISD
+
+#endif	/* MAIN_H */
---
+++ MK-Driver.X/main.h
@@ -0,0 +1,10 @@
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#define LED4_DDR TRISD4
+#define LED4    RD4 
+
+#endif	/* MAIN_H */
+
+
